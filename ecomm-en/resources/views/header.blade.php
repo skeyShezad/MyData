e43354756f58957810d4446f16ac477db210a3b0
skeyShezad <skey.shezadluhar@gmail.com>
2023-03-22 11:24:52 +0530

    e-comm in laravel

---
+++ ecomm-en/resources/views/header.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Brand</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Orders</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-default" type="submit">Submit</button>
+    </form>
+    <ul class="nav navbar-nav navbar-right">
+   
+        <a class="nav-link" href="#">Cart(0)</a>
+      </li>
+  </div>
+</nav>
